160c3ed1e111f844c4c242fa0c6a85d63c74baab
FlavioOliveiraNeto <flavio.neto@goias.gov.br>
2026-02-23 17:48:37 -0300

    Melhora resposta de dados para visualização do paciente

--- app/controllers/clients/sessions_controller.rb
+++ app/controllers/clients/sessions_controller.rb
@@ -4,7 +4,20 @@ class Clients::SessionsController < ApplicationController
 
   def index
     sessions = current_user.sessions.order(:scheduled_at)
-    render json: sessions
+    render json: sessions.map { |session|
+      {
+        id: session.id,
+        scheduled_at: session.scheduled_at,
+        date: session.scheduled_at&.to_date&.iso8601,
+        time: session.scheduled_at&.strftime("%H:%M"),
+        status: session.status,
+        status_label: I18n.t("session.status.#{session.status}", default: session.status.titleize),
+        session_type: session.session_type,
+        google_meet_link: current_user.google_meet_link,
+        created_at: session.created_at,
+        updated_at: session.updated_at
+      }
+    }
   end
 
   private
